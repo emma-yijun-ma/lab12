70faaab3fd20c0fbef30cdc46c0d4dc3cef2807f
edwin yue <edwinyue115@gmail.com>
2023-11-17 10:22:38 -0800

    Updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -38,19 +38,18 @@ public class FamilyTree
         
         // Searches subtree at this node for a node
         // with the given name. Returns the node, or null if not found.
-        TreeNode getNodeWithName(String targetName)
-        {
+        TreeNode getNodeWithName(String targetName) {
             // Does this node have the target name?
             if (this.name.equals(targetName))
                 return this;
-                    
+
             // No, recurse. Check all children of this node.
-            for (TreeNode child: children)
-            {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+            for (TreeNode child : children) {
+                TreeNode result = child.getNodeWithName(targetName);
+                if (result != null)
+                    return result;
             }
-            
+
             // Not found anywhere.
             return null;
         }
@@ -139,7 +138,7 @@ public class FamilyTree
 		}
 		String parent = line.substring(0,colonIndex); // The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
 				           //class java.util.String, method substring(), if you need guidance.
-		String childrenString = line.substring(colonIndex); //The substring of line that starts just after colonIndex and goes through the end of
+		String childrenString = line.substring(colonIndex + 1); //The substring of line that starts just after colonIndex and goes through the end of
 				                   //the line. You'll use a different version of substring().
 		String[] childrenArray = childrenString.split(","); //Call childrenString.split(). Check the API for details. The result will be an array
 				                    //of strings, with the separating commas thrown away.
